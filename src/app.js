0fc93f51b02e834bf68acbf2401e4c12ea92e47f
GoddessD <dlouis314@gmail.com>
2022-10-09 04:29:33 +0000

    update

--- src/app.js
+++ src/app.js
@@ -7,5 +7,17 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let randomNumber = Math.random() * 10;
+  document.querySelector("#theexcuse").innerHTML = randomNumber;
+  console.log("Hello Rigo from the console! " + randomNumber);
+};
+
+let generateEXCUSE = () => {
+  let who = - [ 'She', 'He'. 'They'];
+  let action = ['drove','played', 'moved', 'worked on'];
+  let what = ['']
+  let when = ['']
+
+
+  return " I finished all my coursework by myself";
 };
