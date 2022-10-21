511f648c94d1ed013d8af3964bf4899998930f34
GoddessD <dlouis314@gmail.com>
2022-10-21 22:10:55 +0000

    update

--- src/app.js
+++ src/app.js
@@ -7,17 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  let randomNumber = Math.random() * 10;
-  document.querySelector("#theexcuse").innerHTML = randomNumber;
-  console.log("Hello Rigo from the console! " + randomNumber);
+  document.querySelector("#theexcuse").innerHTML = generateEXCUSE();
 };
-
+let getRandom = limit => {
+  return Math.floor(Math.random() * limit);
+};
 let generateEXCUSE = () => {
-  let who = - [ 'She', 'He'. 'They'];
-  let action = ['drove','played', 'moved', 'worked on'];
-  let what = ['']
-  let when = ['']
+  let who = -["She", "He", "They"];
+  let action = ["broke", "lost", "moved", "ate"];
+  let what = ["the keys", "the computer", "the phone", "the homework"];
+  let when = [" this morning", "this afternoon", "last night", "yesterday"];
 
-
-  return " I finished all my coursework by myself";
+  return (
+    who[getRandom(who.length)] +
+    " " +
+    action[getRandom(action.length)] +
+    " " +
+    what[getRandom(what.length)] +
+    " " +
+    when[getRandom(when.length)]
+  );
 };
